7afad2994080799b0d7074b8b01764c7e49dbf89
Alex Wei <awei@atlassian.com>
2015-05-18 09:59:31 +1000

    Finished and tested up to exercise 3.25 Tree.size

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -7,7 +7,15 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
 
-
+  def size[A](tree: Tree[A]): Int = {
+    @annotation.tailrec
+    def go(ts: List[Tree[A]], acc: Int): Int = ts match {
+      case Nil => acc
+      case Cons(Leaf(_), tail) => go(tail, acc + 1)
+      case Cons(Branch(l, r), tail) => go(Cons(l, Cons(r, tail)), acc + 1)
+    }
+    go(List(tree), 0)
+  }
 
 
 }
