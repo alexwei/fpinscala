3b47d93f4424d8ff358ffbfec35d1f1ded55fdde
Alex Wei <awei@atlassian.com>
2015-05-19 09:26:17 +1000

    Finished and tested up to exercise 3.27 Tree.depth

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -7,6 +7,8 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
 
+  val tree = Branch(Branch(Branch(Branch(Leaf(1), Leaf(2)), Leaf(3)), Branch(Leaf(4), Branch(Leaf(5), Leaf(6)))), Branch(Leaf(7), Branch(Leaf(8), Branch(Leaf(9), Branch(Leaf(10), Branch(Leaf(11), Leaf(12)))))))
+
   def size[A](tree: Tree[A]): Int = {
     @annotation.tailrec
     def go(ts: List[Tree[A]], acc: Int): Int = ts match {
@@ -17,5 +19,24 @@ object Tree {
     go(List(tree), 0)
   }
 
+  def maximum(tree: Tree[Int]): Int = {
+    @annotation.tailrec
+    def go(h: Tree[Int], ts: List[Tree[Int]], result: Option[Int]): Int = (h, ts) match {
+      case (Leaf(i), Nil) => result.getOrElse(i) max i
+      case (Leaf(i), Cons(t, tt)) => go(t, tt, Some(result.getOrElse(i) max i))
+      case (Branch(l, r), _) => go(l, Cons(r, ts), result)
+    }
+    go(tree, Nil, None)
+  }
+
+  def depth[A](tree: Tree[A]): Int = {
+    @annotation.tailrec
+    def go(h: (Tree[A], Int), ts: List[(Tree[A], Int)], result: Option[Int]): Int = (h, ts) match {
+      case ((Leaf(_), d), Nil) => result.getOrElse(d) max d
+      case ((Leaf(_), d), Cons(t, tt)) => go(t, tt, Some(result.getOrElse(d) max d))
+      case ((Branch(l, r), d), _) => go((l, d + 1), Cons((r, d + 1), ts), result)
+    }
+    go((tree, 0), Nil, None)
+  }
 
 }
